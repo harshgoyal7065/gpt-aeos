34f135d4cd13713e8933269a1cea071b761311f5
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2024-06-17 22:06:56 +0530

    Added Team switch

--- src/layout/MainChatArea/MainChatArea.layout.tsx
+++ src/layout/MainChatArea/MainChatArea.layout.tsx
@@ -1,106 +1,133 @@
-"use client"
+"use client";
 
 import Button from "@/components/Button";
+import NameAvatar from "@/components/NameAvatar";
 import TextInput from "@/components/TextInput";
 import { useGptStore } from "@/store";
 import { useState } from "react";
 
 const MainChatArea = () => {
   const [userQuestion, setUserQuestion] = useState("");
-  const updateConversationList = useGptStore((state: any) => state.updateConversationList);
-  const activeTeamDetails = useGptStore((state: any) => state.activeTeamDetails);
-  const activeConversation = useGptStore((state: any) => state.activeConversation);
-  const updateActiveConversation = useGptStore((state: any) => state.updateActiveConversation);
+  const updateConversationList = useGptStore(
+    (state: any) => state.updateConversationList
+  );
+  const activeTeamDetails = useGptStore(
+    (state: any) => state.activeTeamDetails
+  );
+  const activeConversation = useGptStore(
+    (state: any) => state.activeConversation
+  );
+  const updateActiveConversation = useGptStore(
+    (state: any) => state.updateActiveConversation
+  );
+  const user = useGptStore((state: any) => state.user);
 
   const askQuestion = async () => {
     const token = localStorage.getItem("token");
-    let conversationId = '';
+    let conversationId = "";
 
-    if(!activeConversation) {
-      updateConversationList(userQuestion.substring(0,50));
+    if (!activeConversation) {
+      updateConversationList(userQuestion.substring(0, 50));
 
       const createQuestionResponse = await fetch("/api/conversations", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
           team_id: activeTeamDetails.team_id,
-          conversation_title: userQuestion.substring(0,50)
-        })
-      })
+          conversation_title: userQuestion.substring(0, 50),
+        }),
+      });
 
-      if(createQuestionResponse.status === 200) {  
+      if (createQuestionResponse.status === 200) {
         const conversationResponse = await createQuestionResponse.json();
         conversationId = conversationResponse.conversation.id;
       } else {
-        alert('Something went wrong!')
+        alert("Something went wrong!");
         return;
       }
     } else {
       conversationId = activeConversation[0].conversation_id;
     }
 
-      // const response = await fetch("https://api.openai.com/v1/chat/completions", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-      //   },
-      //   body: JSON.stringify({
-      //     model: "gpt-3.5-turbo",
-      //     messages: [{role: "user", content: userQuestion}],
-      //     temperature: 0.7
-      //   })
-      // })
+    // const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+    //   },
+    //   body: JSON.stringify({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [{role: "user", content: userQuestion}],
+    //     temperature: 0.7
+    //   })
+    // })
 
-      if(true) {
-        const createConversationDetailsResponse = await fetch("/api/conversation-details", {
+    if (true) {
+      const createConversationDetailsResponse = await fetch(
+        "/api/conversation-details",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
             question: userQuestion,
-            answer: 'This is a random testing message',
+            answer: "This is a random testing message",
             conversation_id: conversationId,
-            team_id: activeTeamDetails.team_id
-          })
-        })
+            team_id: activeTeamDetails.team_id,
+          }),
+        }
+      );
 
-        if(createConversationDetailsResponse.status === 200) {
-          const res = await createConversationDetailsResponse.json();
-          console.log(res);
-          if(activeConversation) {
-            updateActiveConversation([...activeConversation, {...res.question_answer}]);
-          } else {
-            updateActiveConversation([res.question_answer])
-          }
+      if (createConversationDetailsResponse.status === 200) {
+        const res = await createConversationDetailsResponse.json();
+        if (activeConversation) {
+          updateActiveConversation([
+            ...activeConversation,
+            { ...res.question_answer },
+          ]);
+        } else {
+          updateActiveConversation([res.question_answer]);
         }
       }
-
     }
+  };
 
   return (
     <div className="h-screen flex flex-col justify-between">
-        <div className="flex-grow p-4 overflow-y-auto">
-          <div className="w-10/12 m-auto">
-          {
-            activeConversation?.map((conversation: any) => <>
-            <div className="border border-white-primary text-white-primary p-3 rounded-lg mb-4">{conversation.question}</div>
-            <div className="border border-gray-primary text-white-primary p-3 rounded-lg bg-text-slate-800">{conversation.answer}</div>
-            </>)
-          }
-          </div>
+      <div className="flex justify-between w-10/12">
+      <button>
+        <NameAvatar name={user?.name}/>
+      </button>
+      </div>
+      <div className="flex-grow p-4 overflow-y-auto">
+        <div className="w-10/12 m-auto">
+          {activeConversation?.map((conversation: any) => (
+            <>
+              <div className="border border-white-primary text-white-primary p-3 rounded-lg mb-4">
+                {conversation.question}
+              </div>
+              <div className="border border-gray-primary text-white-primary p-3 rounded-lg bg-text-slate-800 mb-8">
+                {conversation.answer}
+              </div>
+            </>
+          ))}
         </div>
+      </div>
       <div className="w-10/12 m-auto flex gap-5 py-4">
         <div className="flex-[4_1_0%]">
-          <TextInput placeholder="Enter your question" value={userQuestion} onChange={(e) => setUserQuestion(e?.target.value as string)}/>
+          <TextInput
+            placeholder="Enter your question"
+            value={userQuestion}
+            onChange={(e) => setUserQuestion(e?.target.value as string)}
+          />
         </div>
         <div className="flex-[1_1_0%]">
-          <Button text="Submit" onClick={askQuestion}/>
+          <Button text="Submit" onClick={askQuestion} />
         </div>
       </div>
     </div>
--- src/layout/Sidebar/Sidebar.layout.tsx
+++ src/layout/Sidebar/Sidebar.layout.tsx
@@ -1,44 +1,78 @@
-"use client"
+"use client";
 
-import InfoCard from "@/components/InfoCard"
-import Tabs from "@/components/Tabs"
-import { useGptStore } from "@/store"
+import InfoCard from "@/components/InfoCard";
+import Modal from "@/components/Modal";
+import Tabs from "@/components/Tabs";
+import { useGptStore } from "@/store";
+import { useState } from "react";
 
 const Sidebar = () => {
-  const conversationList = useGptStore((state: any) => state.activeTeamDetails.conversationList);
-  const activeTeamDetails = useGptStore((state: any) => state.activeTeamDetails);
+  const conversationList = useGptStore(
+    (state: any) => state.activeTeamDetails.conversationList
+  );
+  const activeTeamDetails = useGptStore(
+    (state: any) => state.activeTeamDetails
+  );
   const user = useGptStore((state: any) => state.user);
-  const updateActiveConversation = useGptStore((state: any) => state.updateActiveConversation);
-  console.log(conversationList, activeTeamDetails);
+  const updateActiveConversation = useGptStore(
+    (state: any) => state.updateActiveConversation
+  );
+  const teamList = useGptStore((state: any) => state.teamList);
+  console.log(teamList);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const setActiveConversation = async (id: any) => {
     const token = localStorage.getItem("token");
-    const createQuestionResponse = await fetch(`/api/conversation-details?conversation_id=${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      },
-    })
+    const createQuestionResponse = await fetch(
+      `/api/conversation-details?conversation_id=${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-    if(createQuestionResponse.status === 200) {
+    if (createQuestionResponse.status === 200) {
       const response = await createQuestionResponse.json();
       updateActiveConversation(response.question_answers);
     }
-
-  }
+  };
 
   return (
     <div className="border rounded-lg border-gray-500 flex flex-col justify-between h-full">
-        <div>
-            <h1 className="text-6xl font-bold text-center p-3">aeogpt.</h1>
-            {conversationList?.map((conversation: any) => <div key={conversation.id}><Tabs text={conversation.conversation_title} handleTabClick={() => setActiveConversation(conversation.id)}/></div>)}
+      <div>
+        <h1 className="text-6xl font-bold text-center p-3">aeogpt.</h1>
+        {conversationList?.map((conversation: any) => (
+          <div key={conversation.id}>
+            <Tabs
+              text={conversation.conversation_title}
+              handleTabClick={() => setActiveConversation(conversation.id)}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="w-11/12 mx-auto p-3">
+        <button onClick={() => setIsModalOpen(true)}>
+          <InfoCard
+            name={user?.name}
+            teamName={activeTeamDetails?.team_name}
+            role={activeTeamDetails?.role_name}
+          />
+        </button>
+      </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Select your team
+        </h3>
+        <div className="mt-2">
+          <p className="text-sm text-gray-500">Your content here.</p>
         </div>
-        <div className="w-11/12 mx-auto p-3">
-            <InfoCard name={user?.name} teamName={activeTeamDetails?.team_name} role={activeTeamDetails?.role_name} />
-        </div>
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
