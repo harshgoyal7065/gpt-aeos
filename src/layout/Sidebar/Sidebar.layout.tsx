85aca1b9f8f0a4668c7a1e93bfd7ebf8c96f36cb
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2024-06-17 19:37:32 +0530

    Added conversation list

--- src/layout/Sidebar/Sidebar.layout.tsx
+++ src/layout/Sidebar/Sidebar.layout.tsx
@@ -8,12 +8,12 @@ const Sidebar = () => {
   const conversationList = useGptStore((state: any) => state.activeTeamDetails.conversationList);
   const activeTeamDetails = useGptStore((state: any) => state.activeTeamDetails);
   const user = useGptStore((state: any) => state.user);
-  console.log(user);
+  console.log(conversationList, activeTeamDetails);
   return (
     <div className="border rounded-lg border-gray-500 flex flex-col justify-between h-full">
         <div>
             <h1 className="text-6xl font-bold text-center p-3">aeogpt.</h1>
-            {conversationList.map((conversation: any) => <div key={conversation}><Tabs text={conversation}/></div>)}
+            {conversationList?.map((conversation: any) => <div key={conversation.id}><Tabs text={conversation}/></div>)}
         </div>
         <div className="w-11/12 mx-auto p-3">
             <InfoCard name={user?.name} teamName={activeTeamDetails?.team_name} role={activeTeamDetails?.role_name} />
